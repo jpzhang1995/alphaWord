057ba2e69bbede356c0021c60b68a6bdfd74e575
sameole <sameole@gmail.com>
2017-03-15 16:42:16 +0800

    dictionary class

---
+++ dictionary.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+# --------------------------------------------------------
+# alphaWord
+# Written by Junpeng Zhang
+# --------------------------------------------------------
+
+
+import os
+import re
+
+
+class Dictionary(object):
+    """
+    a dictionary
+    """
+    
+    def __init__(self, dicPath='dic.txt'):
+        self.wordList=[]
+        if  not os.path.exists(dicPath):
+            print 'can not open '+dicPath
+            return
+        dicFile = open(dicPath,'r')      
+        try:
+            for line in dicFile:
+                #print line
+                line=line.strip('\n')
+                self.wordList.append(line)              
+        except:
+            print 'can not read word list'
+            
+        finally:
+            dicFile.close( )
+            
+    def query(self,suffix):
+         """
+         query all words with  a given prefix from a  word dictionary
+
+         """
+         result =[]
+
+         p1='^'+suffix+r'\S*$'
+         pattern = re.compile(p1) 
+         for word in self.wordList:
+             if pattern.match(word):
+                 result.append(word)
+         return result
+
+
+if __name__ == '__main__':
+    testDic=Dictionary('dic.txt')
+    result=testDic.query('act')
+    print result
